175f11aa49f17f57142e1dd466ecbbe53b6bcd17
Arswarog <arswarog@yandex.ru>
2025-07-29 09:30:24 +0300

    fix(features/evaluate/lexer): добавление возвожности указать, что каждый символ этого типа - отдельный токен

--- src/parser/lexer/tokens.ts
+++ src/parser/lexer/tokens.ts
@@ -3,6 +3,8 @@ import { TokenType } from './types';
 export interface TokenDeclaration {
     type: TokenType;
     chars: string;
+    // каждый символ этого типа - отдельный токен
+    single?: boolean;
 }
 
 export const tokenDeclarations: TokenDeclaration[] = [
@@ -17,29 +19,36 @@ export const tokenDeclarations: TokenDeclaration[] = [
     {
         type: TokenType.PlusOperation,
         chars: '+',
+        single: true,
     },
     {
         type: TokenType.MinusOperation,
         chars: '-',
+        single: true,
     },
     {
         type: TokenType.MultiplyOperation,
         chars: '*×',
+        single: true,
     },
     {
         type: TokenType.DivideOperation,
         chars: '/÷',
+        single: true,
     },
     {
         type: TokenType.HourLiteral,
         chars: 'hHчЧ',
+        single: true,
     },
     {
         type: TokenType.MinuteLiteral,
         chars: 'mMмМ',
+        single: true,
     },
     {
         type: TokenType.SecondLiteral,
         chars: 'sSсС',
+        single: true,
     },
 ];
