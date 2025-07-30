061925f85921ec55d9026cb22971d26b6def7866
e.lubyanoy <e.lubyanoy@tbank.ru>
2025-07-30 12:14:27 +0300

    wip

---
+++ src/parser/parser/__tests__/parser.spec.ts
@@ -0,0 +1,56 @@
+import { describe, expect, it } from 'vitest';
+
+import { HighlightedError, PositionalError } from '../../common';
+import { createToken, TokenType } from '../../lexer';
+import { AttributeNode, RootNode } from '../../nodes';
+import { parse } from '../parser';
+
+describe('Parser', () => {
+    describe('Attribute', () => {
+        it('success: simple attribute', () => {
+            // Arrange
+            const source = '@data-attr';
+
+            // Act
+            const ast = parse(source);
+
+            // Assert
+            expect(ast).toEqual(
+                new RootNode(
+                    new AttributeNode(createToken(TokenType.Attribute, '@data-attr', 0)),
+                    source,
+                ),
+            );
+        });
+        it('failed: unexpected token', () => {
+            // Arrange
+            const source = '@data-attr 123';
+
+            // Act & Assert
+            expect(() => parse(source)).toThrowError(
+                new HighlightedError(
+                    new PositionalError('Unexpected token "xx" (UnknownSymbol)', {
+                        start: 5,
+                        end: 7,
+                    }),
+                    source,
+                ),
+            );
+        });
+        it('failed: unexpected end of file', () => {
+            // Arrange
+            const source = '12+35/';
+
+            // Act & Assert
+            expect(() => parse(source)).toThrowError(
+                new HighlightedError(
+                    new PositionalError('Expected value, got "[EOF]"', {
+                        start: 6,
+                        end: 6,
+                    }),
+                    source,
+                ),
+            );
+        });
+    });
+});
