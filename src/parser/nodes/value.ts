55e44e17daf6e7cf25a117ddd07d369775b4b49a
e.lubyanoy <e.lubyanoy@tbank.ru>
2025-07-30 11:48:46 +0300

    feat(parser/parser): parseValue

---
+++ src/parser/nodes/value.ts
@@ -0,0 +1,28 @@
+import { PositionalError } from '@src/parser';
+
+import { Token, TokenType } from '../lexer';
+
+import { AbstractNode, NodeType } from './abstract';
+
+export class ValueNode extends AbstractNode {
+    public readonly type = NodeType.Value;
+    public readonly value: string;
+    public readonly raw: string;
+
+    constructor(valueToken: Token) {
+        super();
+
+        if (valueToken.type !== TokenType.StringLiteral) {
+            throw new PositionalError(
+                `Expected string literal, got ${valueToken.type}`,
+                valueToken,
+            );
+        }
+
+        this.value = valueToken.text;
+        this.raw = valueToken.text;
+
+        this.start = valueToken.start;
+        this.end = valueToken.end;
+    }
+}
--- src/parser/parser/parser.ts
+++ src/parser/parser/parser.ts
@@ -1,3 +1,5 @@
+import { ValueNode } from '@src/parser/nodes/value.ts';
+
 import { HighlightedError, PositionalError } from '../common';
 import { analyzeCode, Token, TokenType } from '../lexer';
 import { AttributeNode, RootNode } from '../nodes';
@@ -22,7 +24,7 @@ export function parseTokens(tokens: Token[], source: string): RootNode {
 
     const ctx = createContext(tokens);
 
-    const root = new RootNode(parseAttribute(ctx), source);
+    const root = new RootNode(parseValue(ctx), source);
 
     if (!ctx.isEnd()) {
         throw new PositionalError(
@@ -32,6 +34,17 @@ export function parseTokens(tokens: Token[], source: string): RootNode {
     }
 
     return root;
+}
+
+function parseValue(ctx: ParserContext): ValueNode {
+    const value = ctx.getCurrentToken();
+
+    if (value.type === TokenType.StringLiteral) {
+        ctx.next();
+        return new ValueNode(value);
+    }
+
+    throw new PositionalError(`Expected value token, got ${value.type}`, value);
 }
 
 function parseAttribute(ctx: ParserContext): AttributeNode {
