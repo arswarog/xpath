e7c5886570e372ce4a6aae2d4309ba30a126a8a5
Arswarog <arswarog@yandex.ru>
2025-07-31 06:49:03 +0300

    fix(pages/tokens-view): со страницы убрано сообщение об ошибке парсинга

--- src/pages/tokens-view/Page.tsx
+++ src/pages/tokens-view/Page.tsx
@@ -1,17 +1,10 @@
 import { useAtom } from '@reatom/npm-react';
 
-import { astParsingErrorAtom, tokensAtom } from '@src/features/evaluate';
-import { ErrorView } from '@src/widgets/error-view';
+import { tokensAtom } from '@src/features/evaluate';
 import { TokensView } from '@src/widgets/tokens-view';
 
 export function TokensViewPage() {
     const [tokens] = useAtom(tokensAtom);
-    const [error] = useAtom(astParsingErrorAtom);
 
-    return (
-        <>
-            {error && <ErrorView error={error} />}
-            {tokens ? <TokensView tokens={tokens} /> : 'Ошибка парсинга выражения'}
-        </>
-    );
+    return <TokensView tokens={tokens} />;
 }
