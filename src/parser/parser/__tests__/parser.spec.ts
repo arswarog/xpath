f5de3232d44cb2b909fdceace9523a91a281054a
e.lubyanoy <e.lubyanoy@tbank.ru>
2025-07-31 15:46:28 +0300

    fix: исправление parseLogicalExpression

--- src/parser/parser/__tests__/parser.spec.ts
+++ src/parser/parser/__tests__/parser.spec.ts
@@ -4,6 +4,7 @@ import { createToken, TokenType } from '../../lexer';
 import {
     AttributeNode,
     CheckAttributeNode,
+    LogicalExpressionNode,
     PredicateNode,
     RootNode,
     SelectorNode,
@@ -14,7 +15,7 @@ import { parse } from '../parser';
 describe('Parser', () => {
     it('selector and predicate', () => {
         // Arrange
-        const source = './/*[@data-attr="value"]';
+        const source = './/*[@data-attr="value" and @data-attr2="value2"]';
 
         // Act
         const ast = parse(source);
@@ -29,17 +30,35 @@ describe('Parser', () => {
                         new PredicateNode(
                             createToken(TokenType.OpeningSquareBracket, '[', 4),
                             undefined,
-                            new CheckAttributeNode(
-                                new AttributeNode(
-                                    createToken(TokenType.Attribute, '@data-attr', 5),
+                            new LogicalExpressionNode(
+                                new CheckAttributeNode(
+                                    new AttributeNode(
+                                        createToken(TokenType.Attribute, '@data-attr', 5),
+                                    ),
+                                    undefined,
+                                    createToken(TokenType.Equal, '=', 15),
+                                    undefined,
+                                    new ValueNode(
+                                        createToken(TokenType.StringLiteral, '"value"', 16),
+                                    ),
                                 ),
-                                undefined,
-                                createToken(TokenType.Equal, '=', 15),
-                                undefined,
-                                new ValueNode(createToken(TokenType.StringLiteral, '"value"', 16)),
+                                createToken(TokenType.Space, ' ', 23),
+                                createToken(TokenType.And, 'and', 24),
+                                createToken(TokenType.Space, ' ', 27),
+                                new CheckAttributeNode(
+                                    new AttributeNode(
+                                        createToken(TokenType.Attribute, '@data-attr2', 28),
+                                    ),
+                                    undefined,
+                                    createToken(TokenType.Equal, '=', 39),
+                                    undefined,
+                                    new ValueNode(
+                                        createToken(TokenType.StringLiteral, '"value2"', 40),
+                                    ),
+                                ),
                             ),
                             undefined,
-                            createToken(TokenType.ClosingSquareBracket, ']', 23),
+                            createToken(TokenType.ClosingSquareBracket, ']', 48),
                         ),
                     ],
                 ]),
