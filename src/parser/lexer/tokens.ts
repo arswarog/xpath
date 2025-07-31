143bd5b5f282798e1a2c4e0c3ef2c0d54f3f3f98
e.lubyanoy <e.lubyanoy@tbank.ru>
2025-07-31 10:07:57 +0300

    feat(parser/lexer): TokenType.SelectNode 4

--- src/parser/lexer/tokens.ts
+++ src/parser/lexer/tokens.ts
@@ -90,7 +90,7 @@ export const tokenDeclarations: TokenDeclaration[] = [
         type: TokenType.SelectNode,
         chars: /[.*/]/,
         check: /^\.\.?\/?\/?\*?$/,
-        finalCheck: (str) => ['.//*', '..//*'].includes(str),
+        finalCheck: (str) => ['.', '..', './/*', '..//*'].includes(str),
     },
     {
         type: TokenType.SelectNode,
