f5b8c252fedd4457203fe65fea37b16cdec31ac1
e.lubyanoy <e.lubyanoy@tbank.ru>
2025-08-19 11:32:32 +0300

    feat(widgets/navbar): добавление панели навигации

---
+++ src/widgets/navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import block from 'bem-css-modules';
+import { NavLink } from 'react-router';
+
+import styles from './Navbar.module.scss';
+
+const b = block(styles, 'Navbar');
+
+export function Navbar() {
+    return (
+        <nav className={b()}>
+            <ul className={b('list')}>
+                <li className={b('item')}>
+                    <NavLink to="/ast">#ast</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+}
