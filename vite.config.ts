d2b947684f644ac2262bd53b89b5e698c96907df
e.lubyanoy <e.lubyanoy@tbank.ru>
2025-11-28 15:27:12 +0300

    feat(ext): добавление сборки панели devtools

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,12 @@ export default defineConfig({
             '@src': path.resolve(__dirname, 'src'),
         },
     },
+    build: {
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'index.html'),
+                admin: path.resolve(__dirname, 'panel.html'),
+            },
+        },
+    },
 });
